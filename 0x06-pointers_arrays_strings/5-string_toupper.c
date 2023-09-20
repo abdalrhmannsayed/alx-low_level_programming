294524124f700a719c438975ea478d4b0a84f54a
abdalrhmannsayed <abdalrhmannsayed@gmail.com>
2023-09-20 15:24:20 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase letters to uppercase.
+ * @s: analized string.
+ *
+ * Return: String with all letters Uppercased.
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] >= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+	}
+	return (str);
+}
