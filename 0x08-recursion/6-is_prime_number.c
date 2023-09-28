4109c694a0f7d95a086b074692e7d92a14b3a308
abdalrhmannsayed <abdalrhmannsayed@gmail.com>
2023-09-28 06:07:10 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * prime2 - check if n is a prime number
+ * @othern: int
+ * @n: int
+ * Return: 0 or 1
+ */
+
+int check_prime(int n, int othern);
+int is_prime_number(int n)
+{
+	return (check_prime(n, 2));
+}
+
+/**
+ * check_prime - check all number < n if they can divide if
+ * @n: int
+ * @othern: int
+ * Return: int
+ */
+
+int check_prime(int n, int othern)
+{
+	if (othern >= n && n >= 2)
+		return (1);
+	else if (n % othern == 0 || n <= 1)
+		return (0);
+	else
+		return (check_prime(n, othern - 1));
+}
