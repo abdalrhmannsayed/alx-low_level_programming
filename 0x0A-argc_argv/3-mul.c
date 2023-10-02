dd79041a9dc2df389567e2002fae10021d6ca31d
abdalrhmannsayed <abdalrhmannsayed@gmail.com>
2023-10-02 11:11:29 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,25 +3,24 @@
 
 /**
  * main - print sum of 2 numbers.
- * argc: int
- * argv: int
+ * @argc: int
+ * @argv: int
  * Return: 0 success, non-zero fail
  */
 
 int main(int argc, char const *argv[])
 {
-        int sum;
+	int sum;
 
-        if (argc == 3)
-        {
-                sum = atoi(argv[1]) * atoi(argv[2]);
-                printf("%d\n", sum);
-        }
-        else
-        {
-                printf("Error\n");
-                return (1);
-        }
-        return (0);
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }
-
