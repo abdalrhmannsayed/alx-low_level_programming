21511ca0ede7f96b7c885ba203f625450b5b2331
abdalrhmannsayed <abdalrhmannsayed@gmail.com>
2023-10-02 11:32:02 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - it all starts here
@@ -7,20 +8,20 @@
  * Return: Always 0
  */
 
-int main(int argc, char const argv)
+int main(int argc, char **argv)
 {
 	int add = 0;
 	char *c;
 
 	while (argc--)
 	{
-		for (*c = argv[argc]; *c; c++)
+		for (c = argv[argc]; *c; c++)
 		{
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
 		}
-		sum += atoi(argv[argc]);
+		add += atoi(argv[argc]);
 	}
-	printf("%d\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
