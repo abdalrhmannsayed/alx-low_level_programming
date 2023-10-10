f5b463e544227255f4272ff8eb99431d4478757c
abdalrhmannsayed <abdalrhmannsayed@gmail.com>
2023-10-10 11:24:13 -0700

    4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,11 @@
 #ifndef LIKEMACRO_H
 #define LIKEMACRO_H
 /**
- * File: 3-function_like_macro
- * @X: input
+ * File: 4-sum
+ * @x: input
+ * @y: input
  * Desc: header file
  */
-#define ABS(X) ((X) < 0 ? -(1) * (X) : (X))
+#define SUM(x, y) (x + y)
 
 #endif
