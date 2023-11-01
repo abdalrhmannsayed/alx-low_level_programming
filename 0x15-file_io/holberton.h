b38cae87ab6f888dffdb7c6dfb9d57225e08c097
abdalrhmannsayed <abdalrhmannsayed@gmail.com>
2023-11-01 10:50:19 -0700

    update

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,9 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int _putchar(char c);
